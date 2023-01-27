259bd4f9cc78c74f329a635be2e306beb77d2a8b
vaibhav shitole <116602594+VaibhavShitole1303@users.noreply.github.com>
2023-01-27 14:10:33 +0545

    Initial commit

---
+++ app/src/main/java/com/example/app_firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,31 @@
+package com.example.app_firebase;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+        getFirebaseMessage(message.getNotification().getTitle(),
+                message.getNotification().getBody());// here we pass data from the firebase to the notification builder
+    }
+    public void getFirebaseMessage(String title,String msg){
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"myFirebaseChannel")
+                .setSmallIcon(R.drawable.ic_baseline_call_24)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true);
+        // here we use builder to build our notification
+
+
+        //now create notification manager to manage notification
+        NotificationManagerCompat manager=NotificationManagerCompat.from(this);
+        manager.notify(101,builder.build());
+    }
+}
+
